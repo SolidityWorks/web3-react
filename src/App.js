759f1a69772d9da7268287d119b93ee58a6766d8
artemiev <mixartemev@gmail.com>
2022-03-26 14:03:36 +0300

    connect wallet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+
 function App() {
+
+  const [account, setAccount] = useState('Connect Wallet');
+  const {ethereum} = window;
+
+  const checkWalletIsConnected = () => {
+    if (!ethereum) {
+      alert('You need install MetaMask')
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    try {
+      const accounts = await ethereum.request({'method': 'eth_requestAccounts'});
+      setAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const connectWalletButton = (txt) => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>{txt}</button>
+    )
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  })
+
   return (
-    <h1>Hello World</h1>
+    <div className='main-app'>
+      <h1>Web3 React Template</h1>
+      <div>
+        {connectWalletButton(account)}
+      </div>
+    </div>
   );
 }
 export default App;
